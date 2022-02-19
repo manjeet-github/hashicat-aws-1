0f44f110f3dc4f3f3a00fe8b0518b66a28bf3e0a
Yash Khemani <yash@hashicorp.com>
2022-02-19 23:31:41 +0000

    s3 bucket naming conflict - fix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/khemani-training/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket = var.prefix
+  bucket = "${var.prefix}-training-bucket"
   acl    = "private"
 
   versioning = {
