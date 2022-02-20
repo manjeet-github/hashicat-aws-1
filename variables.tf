029658bcf6ab9f351166b89b3f579d6eaefe493a
Yash Khemani <yash@hashicorp.com>
2022-02-20 00:50:51 +0000

    use variable for cidr block

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "ssh_cidr_blocks" {
+variable "owner_cidr_blocks" {
   type = list(string)
   default = ["0.0.0.0/0"]
 }
